2c8dcb2463f494287ad1646cc1404ce5797aa37c
Alejandro Rodriguez <alejandro.joinup@gmail.com>
2024-08-20 13:40:39 +0200

    add context to registerReceiver to prevent native error

--- src/main/java/com/marianhello/bgloc/service/LocationServiceImpl.java
+++ src/main/java/com/marianhello/bgloc/service/LocationServiceImpl.java
@@ -630,7 +630,11 @@ public class LocationServiceImpl extends Service implements ProviderDelegate, Lo
 
     @Override
     public Intent registerReceiver(BroadcastReceiver receiver, IntentFilter filter) {
-        return super.registerReceiver(receiver, filter, null, mServiceHandler);
+        if (Build.VERSION.SDK_INT >= 34 && super.getApplicationInfo().targetSdkVersion >= 34) {
+          return super.registerReceiver(receiver, filter, null, mServiceHandler, Context.RECEIVER_EXPORTED);
+        } else {
+          return super.registerReceiver(receiver, filter, null, mServiceHandler);
+        }
     }
 
     @Override
